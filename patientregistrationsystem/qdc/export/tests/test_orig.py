358b388cd59e2aa220a0dcf78d6cd5a06adb7e34
Mateus <mcostat@outlook.com>
2023-11-13 23:03:04 +0000

    fixed flake8 action

--- patientregistrationsystem/qdc/export/tests/test_orig.py
+++ patientregistrationsystem/qdc/export/tests/test_orig.py
@@ -8,12 +8,11 @@ from unittest.mock import patch
 
 from dateutil.relativedelta import relativedelta
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.test.client import RequestFactory
 from django.urls import reverse
 from django.utils.translation import gettext as _
-
-from custom_user.models import User
 from export.export import is_patient_active
 from export.input_export import InputExport, build_complete_export_structure
 from export.views import QuestionnaireResponse, Questionnaires, Survey, create_directory
@@ -42,9 +41,7 @@ class UtilTests:
         """Cria um participante para ser utilizado durante os testes"""
 
         gender = self.create_gender_mock(gender_name)
-        return Patient.objects.create(
-            name=name, date_birth=date_birth, gender=gender, changed_by=user
-        )
+        return Patient.objects.create(name=name, date_birth=date_birth, gender=gender, changed_by=user)
 
     @staticmethod
     def create_marital_status_mock(marital_status_name="Single") -> MaritalStatus:
@@ -66,9 +63,7 @@ class UtilTests:
 
     @staticmethod
     def create_survey_mock(survey_id, is_initial_evaluation) -> Survey:
-        survey = Survey(
-            lime_survey_id=survey_id, is_initial_evaluation=is_initial_evaluation
-        )
+        survey = Survey(lime_survey_id=survey_id, is_initial_evaluation=is_initial_evaluation)
         survey.save()
 
         return survey
@@ -81,9 +76,7 @@ class UtilTests:
 
         return result["tid"]
 
-    def create_response_survey_mock(
-        self, user, patient, survey, token_id=None
-    ) -> QuestionnaireResponse:
+    def create_response_survey_mock(self, user, patient, survey, token_id=None) -> QuestionnaireResponse:
         if not token_id:
             # TODO (NES-981): maybe this is not necessary. We want just the token id.
             #  It's not necessary to really create participant in LimeSurvey
@@ -101,12 +94,12 @@ class UtilTests:
 
 
 class DirectoryTest(TestCase):
-    user: User
+    user = None
     data: dict[str, Any] = {}
 
     def setUp(self):
         """Cria um participante para ser utilizado durante os testes"""
-        self.user = User.objects.create_user(
+        self.user = get_user_model().objects.create_user(
             username=USER_USERNAME, email="test@dummy.com", password=USER_PWD
         )
         self.user.is_staff = True
@@ -117,9 +110,7 @@ class DirectoryTest(TestCase):
 
         self.client.login(username=USER_USERNAME, password=USER_PWD)
 
-        self.basedir = path.join(
-            path.dirname(path.realpath("__file__")), "test_directory"
-        )
+        self.basedir = path.join(path.dirname(path.realpath("__file__")), "test_directory")
 
         mkdir(self.basedir)
 
@@ -165,12 +156,12 @@ class DirectoryTest(TestCase):
 class PatientActiveTest(TestCase):
     """Cria um participante para ser utilizado durante os testes"""
 
-    user: User
+    user = None
     data: dict[str, Any] = {}
     util = UtilTests()
 
     def setUp(self):
-        self.user = User.objects.create_user(
+        self.user = get_user_model().objects.create_user(
             username=USER_USERNAME, email="test@dummy.com", password=USER_PWD
         )
         self.user.is_staff = True
@@ -246,9 +237,7 @@ class PatientActiveTest(TestCase):
         patient_mock.save()
 
         survey_mock = self.util.create_survey_mock(QUESTIONNAIRE_ID, True)
-        self.util.create_response_survey_mock(
-            self.user, patient_mock, survey_mock, token_id=1
-        )
+        self.util.create_response_survey_mock(self.user, patient_mock, survey_mock, token_id=1)
 
         subject_id = str(float(patient_mock.pk))
         response = is_patient_active(subject_id)
@@ -258,11 +247,11 @@ class PatientActiveTest(TestCase):
 class JsonTest(TestCase):
     """Cria um participante para ser utilizado durante os testes"""
 
-    user = ""
+    user = None
     data: dict[str, Any] = {}
 
     def setUp(self):
-        self.user = User.objects.create_user(
+        self.user = get_user_model().objects.create_user(
             username=USER_USERNAME, email="test@dummy.com", password=USER_PWD
         )
         self.user.is_staff = True
@@ -283,12 +272,12 @@ class JsonTest(TestCase):
 class InputExportTest(TestCase):
     """Cria um participante para ser utilizado durante os testes"""
 
-    user = ""
+    user = None
     data: dict[str, Any] = {}
     util = UtilTests()
 
     def setUp(self):
-        self.user = User.objects.create_user(
+        self.user = get_user_model().objects.create_user(
             username=USER_USERNAME, email="test@dummy.com", password=USER_PWD
         )
         self.user.is_staff = True
@@ -321,9 +310,7 @@ class InputExportTest(TestCase):
 
         self.assertNotIn("participants", input_data.data)
 
-        input_data.build_diagnosis_participant(
-            "participants", "Participants", participant_field_header_list
-        )
+        input_data.build_diagnosis_participant("participants", "Participants", participant_field_header_list)
 
         self.assertIn("participants", input_data.data)
 
@@ -333,9 +320,7 @@ class InputExportTest(TestCase):
 
         self.assertNotIn("questionnaires", input_data.data)
 
-        input_data.build_questionnaire(
-            questionnaire_list, "pt-BR", entrance_questionnaire=True
-        )
+        input_data.build_questionnaire(questionnaire_list, "pt-BR", entrance_questionnaire=True)
 
         self.assertIn("questionnaires", input_data.data)
 
@@ -407,12 +392,12 @@ class InputExportTest(TestCase):
 class AdvancedSearchTest(TestCase):
     """Cria um participante para ser utilizado durante os testes"""
 
-    user = ""
+    user = None
     data: dict[str, Any] = {}
     util = UtilTests()
 
     def setUp(self):
-        self.user = User.objects.create_user(
+        self.user = get_user_model().objects.create_user(
             username=USER_USERNAME, email="test@dummy.com", password=USER_PWD
         )
         self.user.is_staff = True
@@ -451,17 +436,13 @@ class AdvancedSearchTest(TestCase):
         for index in range(6):
             patient = Patient.objects.get(name=self._testMethodName + str(index))
             marital_status_id = int(index / 2.5)
-            marital_status = MaritalStatus.objects.get(
-                name=marital_status_list[marital_status_id]
-            )
+            marital_status = MaritalStatus.objects.get(name=marital_status_list[marital_status_id])
             patient.marital_status = marital_status
             patient.save()
 
     def test_filter_all_participants(self):
         for index in range(3):
-            self.util.create_patient_mock(
-                name=self._testMethodName + str(index), user=self.user
-            )
+            self.util.create_patient_mock(name=self._testMethodName + str(index), user=self.user)
 
         response = self.client.get(reverse("filter_participants"))
         self.assertEqual(response.status_code, 200)
@@ -522,9 +503,7 @@ class AdvancedSearchTest(TestCase):
             birthday_max.day,
         )
 
-        self.assertEqual(
-            len(response.wsgi_request.session["filtered_participant_data"]), 3
-        )
+        self.assertEqual(len(response.wsgi_request.session["filtered_participant_data"]), 3)
 
     def test_filter_marital_status_participants(self):
         self.create_initial_patients_data()
