8ff6d952af6324d5ebc98491b2d55431176ad67d
Mateus <mcostat@outlook.com>
2023-12-02 01:48:58 +0000

    entrypoint

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -23,7 +23,7 @@ sh "$NES_DIR/scripts/setup_nes.sh" vscode
 echo "INFO: Done initializing data"
 
 echo "INFO: Starting Redis"
-sudo service redis-server start
+service redis-server start
 
 echo "INFO: entrypoint.sh finished"
 
